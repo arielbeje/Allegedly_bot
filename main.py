dd6ea27eaf16fd8c4588029732072d37173181d1
arielbeje <arielb624@gmail.com>
2017-07-07 18:44:31 +0300

    Fixed minor issue in main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ YourID = ""
 
 @client.event
 async def on_message (message):
-	if message.author. == YourID:
+	if message.author.id == YourID:
 		int = randint(0, 99)
 		if int = 0:
 			await client.send_message(message.channel, "*Allegedly...*")
